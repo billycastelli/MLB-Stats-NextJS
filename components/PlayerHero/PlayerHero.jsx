5c2d3953c94d8b067232b380121d4979ccf28ebb
Billy Castelli <billycastelli@gmail.com>
2021-02-02 21:12:55 -0800

    Player page mobile styling

--- components/PlayerHero/PlayerHero.jsx
+++ components/PlayerHero/PlayerHero.jsx
@@ -3,41 +3,43 @@ import styles from "./PlayerHero.module.scss";
 const PlayerHero = (props) => {
   return (
     <div className="container">
-      <div className={styles.banner}>
-        <h1 className={styles.playerName}>{props.data.name}</h1>
-        <hr />
-        <div className="columns">
-          <div className="column is-half">
-            <p>Bats: {props.data.bats}</p>
-            <p>
-              DOB: {props.data.birthmonth}/{props.data.birthday}/
-              {props.data.birthyear}
-            </p>
-            <p>
-              From: {props.data.birthcity}, {props.data.irthcountry}
-            </p>
-          </div>
-          <div className="column is-one-third">
-            <div className={styles.quickStats}>
-              <div className={styles.quickStatsTitle}>
-                <span>
-                  Highlights <img src="/images/emoji_events-24px.svg" />
-                </span>
-              </div>
-              <div className="columns">
-                <div className="column ">
-                  <p className={styles.highlightLabel}>HITS</p>
-                  <p>{props.data.career_batting.hits}</p>
+      <div className={`columns is-centered is-mobile ${styles.banner}`}>
+        <div className="column is-four-fifths-desktop is-four-fifths-tablet is-full-mobile">
+          <h1 className={styles.playerName}>{props.data.name}</h1>
+          <hr />
+          <div className="columns">
+            <div className="column is-half">
+              <p>Bats: {props.data.bats}</p>
+              <p>
+                DOB: {props.data.birthmonth}/{props.data.birthday}/
+                {props.data.birthyear}
+              </p>
+              <p>
+                From: {props.data.birthcity}, {props.data.irthcountry}
+              </p>
+            </div>
+            <div className="column is-one-third">
+              <div className={styles.quickStats}>
+                <div className={styles.quickStatsTitle}>
+                  <span>
+                    Highlights <img src="/images/emoji_events-24px.svg" />
+                  </span>
                 </div>
-                <div className="column">
-                  <p className={styles.highlightLabel} H>
-                    HR
-                  </p>
-                  <p>{props.data.career_batting.homeruns}</p>
-                </div>
-                <div className="column">
-                  <p className={styles.highlightLabel}>AVG</p>
-                  <p>{props.data.career_batting.avg}</p>
+                <div className="columns">
+                  <div className="column">
+                    <p className={styles.highlightLabel}>HITS</p>
+                    <p>{props.data.career_batting.hits}</p>
+                  </div>
+                  <div className="column">
+                    <p className={styles.highlightLabel} H>
+                      HR
+                    </p>
+                    <p>{props.data.career_batting.homeruns}</p>
+                  </div>
+                  <div className="column">
+                    <p className={styles.highlightLabel}>AVG</p>
+                    <p>{props.data.career_batting.avg}</p>
+                  </div>
                 </div>
               </div>
             </div>
