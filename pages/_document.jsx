c96f5fa2247c56e073ad9db7afe949215731f6c6
Billy Castelli <billycastelli@gmail.com>
2021-05-26 20:17:11 -0700

    Add baseball favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/images/baseball-favicon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
